3ccceca73521ec8c9566b8a36e881a81ec273620
elbs8845 <elbs8845@gmail.com>
2026-01-05 21:28:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-// ===== Usuários iniciais =====
-let users = JSON.parse(localStorage.getItem('users')) 
-  || [
-  {login:'admin', senha:'1234', tipo:'Admin'}
-];
-
-// ===== Botão Entrar =====
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('btnEntrar');
-  if(btn) {
-    btn.addEventListener('click', () => {
-      const user = document.getElementById('user').value;
-      const pass = document.getElementById('pass').value;
-      const error = document.getElementById('error');
+  const error = document.getElementById('error');
 
-      const encontrado = users.find(u => u.login === user && u.senha === pass);
-      if(encontrado) {
-        localStorage.setItem('usuarioAtual', JSON.stringify(encontrado));
-        window.location.href = 'dashboard.html';
-      } else {
-        error.textContent = 'Usuário ou senha incorretos';
-      }
-    });
-  }
+  btn.addEventListener('click', () => {
+    const user = document.getElementById('user').value;
+    const pass = document.getElementById('pass').value;
+
+    console.log("Clicou! Usuário:", user, "Senha:", pass); // TESTE
+    if(user === "admin" && pass === "1234") {
+      window.location.href = "dashboard.html";
+    } else {
+      error.textContent = "Usuário ou senha incorretos";
+    }
+  });
 });
