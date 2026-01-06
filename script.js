54bd1acf161ac11d31bd0197f86cf12a6a4ba413
elbs8845 <elbs8845@gmail.com>
2026-01-05 21:32:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('btnEntrar');
-  const error = document.getElementById('error');
+const users = [
+  {login: "admin", senha: "1234", tipo: "Admin"}
+];
 
-  btn.addEventListener('click', () => {
-    const user = document.getElementById('user').value;
-    const pass = document.getElementById('pass').value;
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnEntrar");
+  const error = document.getElementById("error");
 
-    console.log("Clicou! Usuário:", user, "Senha:", pass); // TESTE
-    if(user === "admin" && pass === "1234") {
+  btn.addEventListener("click", () => {
+    const user = document.getElementById("user").value.trim();
+    const pass = document.getElementById("pass").value.trim();
+
+    if(user === "" || pass === "") {
+      error.textContent = "Preencha todos os campos";
+      return;
+    }
+
+    const encontrado = users.find(u => u.login === user && u.senha === pass);
+
+    if(encontrado) {
+      // Redireciona para dashboard
       window.location.href = "dashboard.html";
     } else {
       error.textContent = "Usuário ou senha incorretos";
